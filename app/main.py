ec15ad54fbebe192a36e61d36e5020da38086ed4
fredcode <fredcode23@gmail.com>
2020-03-18 02:27:21 +0800

    query questions in homepage

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from flask import Flask, Blueprint, redirect, render_template, request, flash, jsonify, url_for, abort
 from flask_login import LoginManager, login_required, current_user
-from app.models import User
+from app.models import User, Questions
 from app import db
 
 
 main = Blueprint('main', __name__)
 
 
-@main.route("/")
+@main.route("/", methods=['POST','GET'])
 def home():
-  return(render_template("home.html"))
+  if request.method == 'POST':
+    selection = request.form.get("selection")
+    #show questions
+    if selection == "UQ":
+      questions= Questions.query.filter_by(answer=" ").all()
+    elif selection == "AQ":
+      pass
+    elif selection == "ALQ":
+      questions= Questions.query.all()
+
+    return(render_template("home.html", questions=questions))
+  else:
+    questions= Questions.query.all()
+    return(render_template("home.html",questions=questions))
+
